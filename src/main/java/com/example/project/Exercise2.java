0b3c3909dcc88f00bdb49301fa19f35eb7b8e08f
AnthonyTH <90292092+AnthonyMTH@users.noreply.github.com>
2022-07-13 20:46:50 -0500

    Update Exercise2.java

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -21,7 +21,41 @@ public class Exercise2 {
 
 
     public <T extends Comparable<T>> boolean bstIguales(BST<T> a1, BST<T> a2){
+        int equalsLeft = 0;
+        int equalsRight = 0;
+        Node<T> auxNode = a1.root, auxNode2 = a2.root;
+        if (auxNode != null && auxNode2 != null) {
+            for (; auxNode != null ||
+                    auxNode2 != null; auxNode = auxNode.left, auxNode2 = auxNode2.left) {
+                    if (auxNode.data.compareTo(auxNode2.data) != 0) return false;
+                    if (auxNode.left == null && auxNode2.left != null
+                    || auxNode.right != null && auxNode2.right == null) {
+                    return false;
+                }
+                if (auxNode.left == null && auxNode2.left == null
+                    || auxNode.right == null && auxNode2.right == null) continue;
+                equalsLeft = auxNode.left.data.compareTo(auxNode2.left.data);
+                equalsRight = auxNode.right.data.compareTo(auxNode2.right.data);
+                if (equalsLeft != 0 || equalsRight != 0) return false;
+            }
 
+            auxNode = a1.root; 
+            auxNode2 = a2.root;
+            for (; auxNode != null ||
+                    auxNode2 != null; auxNode = auxNode.right, auxNode2 = auxNode2.right) {
+                if (auxNode.data.compareTo(auxNode2.data) != 0) return false; 
+                if (auxNode.left == null && auxNode2.left != null
+                    || auxNode.right != null && auxNode2.right == null) {
+                    return false;
+                }
+                if (auxNode.left == null && auxNode2.left == null
+                    || auxNode.right == null && auxNode2.right == null) continue;
+                equalsLeft = auxNode.left.data.compareTo(auxNode2.left.data);
+                equalsRight = auxNode.right.data.compareTo(auxNode2.right.data);
+                if (equalsLeft != 0 || equalsRight != 0) return false;
+            }
+            return true;
+        } else if (auxNode == null && auxNode2 == null) return true;
         return false;
     }
 }
